87c72e045f7ad759ac530048623da476ae07b5f1
Andrew Hale <andrew.hale003@gmail.com>
2024-03-17 11:29:43 +1000

    neotree auto close

--- lua/kickstart/plugins/neo_tree.lua
+++ lua/kickstart/plugins/neo_tree.lua
@@ -10,6 +10,15 @@ return {
 		'MunifTanjim/nui.nvim',
 	},
 	config = function()
-		require('neo-tree').setup {}
+		require('neo-tree').setup {
+			event_handlers = {
+				{
+					event = 'file_opened',
+					handler = function()
+						require('neo-tree.command').execute { action = 'close' }
+					end,
+				},
+			},
+		}
 	end,
 }
