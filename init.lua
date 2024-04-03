37b746f0633c2dc99ecc24ae030e78b46861d074
Andrew Hale <2282836@cognizant.com>
2024-04-03 13:06:11 +1000

    markdown preview

--- init.lua
+++ init.lua
@@ -615,6 +615,15 @@ require('lazy').setup({
 	require 'kickstart.plugins.neo_tree',
 	require 'kickstart.plugins.autopairs',
 
+	{
+		'iamcco/markdown-preview.nvim',
+		cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+		ft = { 'markdown' },
+		build = function()
+			vim.fn['mkdp#util#install']()
+		end,
+	},
+
 	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
 	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 	{ import = 'custom.plugins' },
